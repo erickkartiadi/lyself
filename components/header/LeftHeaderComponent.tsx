fed04709876ed6b03efaa4e4a26e5b598a8b9be3
erickkartiadi <erickcartiady@gmail.com>
2022-07-14 13:55:36 +0700

    :sparkles: dynamic greeting

--- components/header/LeftHeaderComponent.tsx
+++ components/header/LeftHeaderComponent.tsx
@@ -1,11 +1,19 @@
 import { Text } from '@rneui/themed';
 import React from 'react';
 import { View } from 'react-native';
+import dayjs from 'dayjs';
 
 function LeftHeaderComponent() {
+  const currentHour = dayjs().hour();
+  let greeting = 'Howdy';
+
+  if (currentHour >= 5 && currentHour <= 11) greeting = 'Good morning';
+  else if (currentHour >= 12 && currentHour <= 16) greeting = 'Good afternoon';
+  else greeting = 'Good night';
+
   return (
     <View>
-      <Text>Good morning</Text>
+      <Text>{greeting}</Text>
       <Text h1>Erick Kartiadi</Text>
     </View>
   );
