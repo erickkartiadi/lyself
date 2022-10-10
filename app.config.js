b91c67911103c601b33cd1f848b9e32189612389
erickkartiadi <erickcartiady@gmail.com>
2022-10-10 11:02:18 +0700

    :wrench: change notification icon & dynamic env

--- app.config.js
+++ app.config.js
@@ -1,5 +1,32 @@
 import 'dotenv/config';
 
+const Config = {
+  redirectUri: process.env.REDIRECT_URI,
+
+  firebaseApiKey: process.env.FIREBASE_API_KEY,
+  firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+  firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  firebaseAppId: process.env.FIREBASE_APP_ID,
+  firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  spotifyTokenKey: process.env.SPOTIFY_TOKEN_KEY,
+  spotifyRefreshTokenKey: process.env.SPOTIFY_REFRESH_TOKEN_KEY,
+  spotifyExpiresTimeKey: process.env.SPOTIFY_EXPIRES_TIME_KEY,
+
+  newsApiKey: process.env.NEWS_API_KEY,
+
+  apiBaseUrl: process.env.API_BASE_URL,
+  userTokenKey: process.env.USER_TOKEN_KEY,
+};
+
+if (process.env.APP_ENV === 'development') {
+  Config.redirectUri = 'exp://192.168.0.128:19000';
+}
+
 export default {
   expo: {
     name: 'lyself',
@@ -42,34 +69,13 @@ export default {
       [
         'expo-notifications',
         {
-          icon: './assets/adaptive-icon.png',
+          icon: './assets/icon-transparent.png',
           color: '#ffffff',
         },
       ],
     ],
     extra: {
-      mode: 'development',
-      redirectUri: process.env.REDIRECT_URI,
-
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-
-      spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-      spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      spotifyTokenKey: process.env.SPOTIFY_TOKEN_KEY,
-      spotifyRefreshTokenKey: process.env.SPOTIFY_REFRESH_TOKEN_KEY,
-      spotifyExpiresTimeKey: process.env.SPOTIFY_EXPIRES_TIME_KEY,
-
-      newsApiKey: process.env.NEWS_API_KEY,
-
-      apiBaseUrl: process.env.API_BASE_URL,
-      userTokenKey: process.env.USER_TOKEN_KEY,
-
+      ...Config,
       eas: {
         projectId: '78d64a9e-b2e7-4f52-9684-5009b95981e7',
       },
